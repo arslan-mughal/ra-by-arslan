119e637024ab9f542bd61116f3ddb126f3154d45
Muhammad Arslan Bashir <66203233+arslan-mughal@users.noreply.github.com>
2020-06-06 18:01:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br />
+        <p>Hello, World! By Muhammad Arslan Bashir.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
